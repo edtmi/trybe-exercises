61c1fac1e13994ae0476de33c66112d93b9206e9
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-02 20:48:13 -0300

    docs: exercise 8 solved

---
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-02-javascript-array-and-loop-for/04 - exercise of the day/08 - exercicio.js
@@ -0,0 +1,8 @@
+let numbers = [];
+
+// 8- Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+for (let index = 1; index <= 25; index += 1) {
+  numbers.push(index)
+}
+console.log(numbers);
