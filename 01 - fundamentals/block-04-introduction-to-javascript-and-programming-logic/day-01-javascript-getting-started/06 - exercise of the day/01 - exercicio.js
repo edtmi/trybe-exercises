7a6684329f870c6b4da5a99894aa72aaf5b3051e
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-02 18:32:27 -0300

    docs: exercise 1 solved

---
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/06 - exercise of the day/01 - exercicio.js
@@ -0,0 +1,30 @@
+/* 1. Faça cinco programas, um para cada operação aritmética básica. 
+Seu programa deve ter duas constantes, a e b , definidas no começo 
+com os valores que serão operados. Faça programas para:
+-Adição (a + b)
+-Subtração (a - b)
+-Multiplicação (a * b)
+-Divisão (a / b)
+-Módulo (a % b)
+*/
+// Adição
+const a = 8;
+const b = 5;
+const sum = a+b;
+console.log('Addition: ',sum);
+
+//Subtração
+const sub = a-b;
+console.log('Subtraction: ',sub);
+
+//Multiplicação
+const mult = a*b
+console.log('Multiplication: ',mult);
+
+//Divisão
+const div = a/b
+console.log('Division: ',div);
+
+//Módulo
+const mod = a%b
+console.log('Mod: ',mod);
