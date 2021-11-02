c4a6cee98fe3d201b5f5d90258544655788303db
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-02 18:18:16 -0300

    docs: exercise 2 solved

--- 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/02 - primitive types, dynamic typing and arithmetic operations/script.js
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/02 - primitive types, dynamic typing and arithmetic operations/script.js
@@ -2,3 +2,10 @@
 e atribua os seus respectivos valores: 5 e 8. */
 const base = 5;
 let height = 8;
+
+/* 2. Crie uma constante chamada area e atribua a ela o resultado 
+da multiplicação da base pela altura. Dica: lembre-se de usar 
+sempre o console.log() para imprimir as variáveis e checar os 
+resultados das operações! */
+const area = base * height;
+console.log('The base area'+ base +' and height '+ height +' is: '+ 'area');
