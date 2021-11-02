2ae28fbb64b6e30916a43476d53b9e8aa0c0e6e4
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-02 18:22:08 -0300

    docs: exercise 2, 3 and 4 solveds

--- 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/03 - if-else condition/script.js
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/03 - if-else condition/script.js
@@ -1,3 +1,25 @@
 /* 1. Crie uma constante que receba a nota de uma pessoa candidata 
 em um desafio técnico, e atribua a ela um valor entre 1 e 100; */
 const grade = 50;
+
+/* 2. Implemente uma lógica que verifique se a pessoa candidata foi 
+aprovada, reprovada ou se ela está na lista de espera. Para isso, 
+considere as seguintes informações:
+   Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
+   Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na 
+nossa lista de espera"
+   Se a nota for menor que 60, imprima "Você foi reprovada(o) 
+   3. Crie uma estrutura condicional utilizando o if , else if e else 
+para criar o seu algoritmo, e os operadores lógicos que se aplicam a 
+cada situação.
+   4.Altere o valor da nota para verificar se as condições que você 
+implementou funcionam; */
+if (grade >= 80) {
+  console.log ('Congratulations, you have been approved!');
+}
+else if (grade >= 60 && grade < 80) {
+  console.log('You are on our waiting list');
+}
+else {
+  console.log('You have failed');
+}
