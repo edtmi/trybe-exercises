712573b84cf2b205f232f24d075d354a3fd1ad34
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-02 18:33:48 -0300

    docs: exercise 7 solved

---
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/06 - exercise of the day/07 - exercicio.js
@@ -0,0 +1,34 @@
+/* 7. Escreva um programa que converte uma nota dada em porcentagem 
+(de 0 a 100) em conceitos de A a F. Siga essas regras:
+Porcentagem >= 90 -> A
+Porcentagem >= 80 -> B
+Porcentagem >= 70 -> C
+Porcentagem >= 60 -> D
+Porcentagem >= 50 -> E
+Porcentagem < 50 -> F
+O programa deve retornar uma mensagem de erro e encerrar se a nota 
+passada for menor que 0 ou maior que 100. */
+
+const grade = 101;
+
+if (grade>100 || grade <0) {
+  console.log('Grade not exist!');
+}
+else if (grade >= 90) {
+  console.log('A')
+}
+else if (grade >= 80) {
+  console.log('B');
+}
+else if (grade >= 70) {
+  console.log('C');
+}
+else if (grade >= 60) {
+  console.log('D');
+}
+else if (grade >= 50) {
+  console.log('E');
+}
+else {
+  console.log('F');
+}
