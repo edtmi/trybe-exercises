bdb071b223d95782c0afb0e7bab91749a6e83e39
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-02 18:27:15 -0300

    docs: exercise 2 solved

--- 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/05 - swich and case/script.js
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/05 - swich and case/script.js
@@ -1,3 +1,24 @@
 /* 1. Crie uma variável para armazenar o estado da pessoa candidata no 
 processo seletivo, que pode ser: 'aprovada' , 'lista' ou 'reprovada' ; */
 let status = 'approved'
+
+/* 2. Crie uma estrutura condicional com o switch/case que irá imprimir 
+as mensagens do exercício anterior se o estado da pessoa candidata 
+for 'aprovada' , 'lista' ou 'reprovada' . Como default , imprima a 
+mensagem de 'não se aplica' .*/
+switch (status) {
+  case 'approved':
+    console.log('approved');
+    break;
+
+  case 'list':
+    console.log('list');
+    break;
+  
+  case 'failed':
+    console.log('failed');
+    break;
+
+  default:
+    console.log('not applicable');
+}
