efa845ba32f7310a04639a3ec8564bc4c79676a3
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-02 20:49:01 -0300

    docs: exercise 02 solved

---
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-02-javascript-array-and-loop-for/05 - bonus/02 - exercicio.js
@@ -0,0 +1,14 @@
+// 2-Ordene o array numbers em ordem decrescente e imprima seus valores;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[secondIndex] < numbers[index]) {
+      let aux = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = aux;
+    }
+  }   
+}
+console.log(numbers);
