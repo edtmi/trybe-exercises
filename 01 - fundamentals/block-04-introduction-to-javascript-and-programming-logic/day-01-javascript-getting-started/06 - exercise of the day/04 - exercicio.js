e6ec70344168765435c64281ca3f33f44ef98293
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-02 18:33:16 -0300

    docs: exercise 4 solved

---
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/06 - exercise of the day/04 - exercicio.js
@@ -0,0 +1,14 @@
+/* 4. Faça um programa que, dado um valor definido numa constante, 
+retorne "positive" se esse valor for positivo, "negative" se for negativo 
+e "zero" caso contrário. */
+const value = -1;
+
+if (value > 0) {
+  console.log('positive');
+}
+else if (value < 0) {
+  console.log('negative');
+}
+else {
+  console.log('zero');
+}
