1c9c934e4fdd5b37e247b93d435ff2883e3205cf
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-02 18:15:19 -0300

    docs: exercise 2 solved

--- 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/01 - variables/script.js
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/01 - variables/script.js
@@ -1,3 +1,7 @@
 /* 1. Crie uma constante chamada myName e atribua a ela o seu 
 nome (Exemplo: Carolina). */
 const myName = "Eduardo";
+
+/* 2. Crie uma constante chamada birthCity e atribua a ela a sua 
+cidade natal. */
+const birthCity = "Mogi das Cruzes";
