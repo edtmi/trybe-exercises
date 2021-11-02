2e17bac67ceba9065809853e5bd715168393be6c
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-02 18:15:40 -0300

    docs: exercise 3 solved

--- 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/01 - variables/script.js
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-01-javascript-getting-started/01 - variables/script.js
@@ -5,3 +5,7 @@ const myName = "Eduardo";
 /* 2. Crie uma constante chamada birthCity e atribua a ela a sua 
 cidade natal. */
 const birthCity = "Mogi das Cruzes";
+
+/* 3. Crie uma variável chamada birthYear e atribua a ela o ano 
+em que você nasceu. */
+let birthYear = 1989;
