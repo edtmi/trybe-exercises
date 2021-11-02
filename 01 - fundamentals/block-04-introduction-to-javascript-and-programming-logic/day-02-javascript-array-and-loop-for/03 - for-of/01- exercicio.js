b40cf12bc2156f07fe007a0a5e8c27693778d187
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-11-02 20:46:35 -0300

    docs: exercise 1 solved

---
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-02-javascript-array-and-loop-for/03 - for-of/01- exercicio.js
@@ -0,0 +1,8 @@
+/* 1.Utilize o for/of para imprimir os elementos da lista names com o 
+console.log() : */
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let index of names) {
+  console.log(index);
+}
