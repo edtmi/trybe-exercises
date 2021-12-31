21f46fa34cf9a8de7c543e2dfbb0841c8893d7cb
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-12-31 16:48:55 -0300

    docs: exercise 7 solved

---
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-04-javascript-objects-and-functions/04 - exercises of the day/01 - part I - objects and for-in/07 - exercicio.js
@@ -0,0 +1,30 @@
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+// 7 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações:
+
+// {
+//   titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//   autor: 'JK Rowling',
+//   editor: 'Rocco',
+// }
+
+leitor.livrosFavoritos.push(
+  {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editor: 'Rocco',
+  }
+)
+
+console.log(leitor.livrosFavoritos);
