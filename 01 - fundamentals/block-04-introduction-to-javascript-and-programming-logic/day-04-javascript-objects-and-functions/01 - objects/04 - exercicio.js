e24f83e35c1c7f9631bd2e94b4132b167c9ea93b
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-12-31 16:46:04 -0300

    docs: exercise 4 solved

---
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-04-javascript-objects-and-functions/01 - objects/04 - exercicio.js
@@ -0,0 +1,5 @@
+/* 4 - Acesse a chave bestInTheWorld e faça um 
+console.log no seguinte formato: 
+"A jogadora Marta Silva foi eleita a melhor do mundo 
+por 6 vezes".*/
+console.log('A jogadora ' + person.name + ' '+ person.lastName +' foi eleita a melhor do Mundo por '+ person.bestInTheWorld.length +' vezes.');
