3bafce34b7ae4e71916c8c81ad1cc7fc5c8c67e5
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-12-31 16:48:49 -0300

    docs: exercise 6 solved

---
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-04-javascript-objects-and-functions/04 - exercises of the day/01 - part I - objects and for-in/06 - exercicio.js
@@ -0,0 +1,18 @@
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+// 6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+console.log('O livro favorito de ' + leitor.nome + 'se chama "' + leitor.livrosFavoritos[0].titulo + '"');
+
+console.log('O livro favorito de ' + leitor['nome'] + 'se chama "' + leitor['livrosFavoritos'][0].titulo + '"');
