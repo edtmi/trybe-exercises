0d0466b0c0b46fd657153daaaf1f08daf6cd6dc4
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-12-31 16:45:34 -0300

    docs: exercise 1 solved

---
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-04-javascript-objects-and-functions/01 - objects/01 - exercicio.js
@@ -0,0 +1,15 @@
+/* 1 - Crie um objeto player contendo as variáveis 
+listadas abaixo.*/
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+let person = {
+  name : 'Marta',
+  lastName : 'Silva',
+  age : 34,
+  medals : { 
+    golden: 2, 
+    silver:3,
+  },    
+}
