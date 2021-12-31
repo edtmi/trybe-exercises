f663ae2bf8b3067f3090aa75012e8aad3d16b2a9
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-12-31 16:46:10 -0300

    docs: exercise 5 solved

---
+++ 01 - fundamentals/block-04-introduction-to-javascript-and-programming-logic/day-04-javascript-objects-and-functions/01 - objects/05 - exercicio.js
@@ -0,0 +1,7 @@
+/* 5 - Acesse a chave medals e faça um console.log no 
+seguinte formato: "A jogadora possui 2 medalhas de 
+ouro e 3 medalhas de prata".
+Agora veremos uma variação do laço for , que nos 
+garante facilidade ao lidar com objetos.
+ */
+console.log('A jogadora possui '+ person.medals.golden +' medalhas de ouro e ' + person.medals.silver + ' medalhas de prata.');
